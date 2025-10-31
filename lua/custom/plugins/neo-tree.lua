547e626c5fb1c6cefb830a9e6a423c7f74b4f6a7
MaasOedipa <MaasOedipa@github.com>
2025-10-31 14:37:10 +0100

    neotree moved to custom plugins, to config more personally

---
+++ lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,35 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  auto_close = true,
+  auto_open = false,
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = true,
+        hide_gitignored = true,
+      },
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+        position = 'left',
+        width = '20',
+      },
+      hijack_netrw_behavior = 'open_default',
+    },
+  },
+}
