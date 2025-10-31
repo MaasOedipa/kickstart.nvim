add56d174da053801e74aeae3ee83226f26bb197
MaasOedipa <MaasOedipa@github.com>
2025-10-31 11:20:46 +0100

    transparent background on theme

--- lua/custom/themes/catppuccin.lua
+++ lua/custom/themes/catppuccin.lua
@@ -1,13 +1,16 @@
 return function()
   -- setup Catppuccin
   require('catppuccin').setup {
-    flavour = 'mocha', -- options: latte, frappe, macchiato, mocha
+    flavour = 'macchiato', -- options: latte, frappe, macchiato, mocha
     integrations = {
       -- Optional: enable integrations for plugins like telescope, lualine, etc
       telescope = true,
       nvimtree = true,
       cmp = true,
+      mason = true,
+      treesitter = true,
     },
+    transparent_background = true,
   }
   -- load the colorscheme
   vim.cmd.colorscheme 'catppuccin'
